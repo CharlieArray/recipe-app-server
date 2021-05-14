6797ccdbf0fda5bc7322b68e2b64d68cc206d6a2
Charles Ybarra <charles.ybarra@outlook.com>
2021-05-14 14:04:04 -0500

    updating config files

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-     DB_URL: process.env.DATABASE_URL || "postgresql://postgres@localhost/recipe-app",
+     DB_URL: process.env.DATABASE_URL || "postgresql://postgres@localhost/grocery-app",
      JWT_SECRET: process.env.JWT_SECRET || 'key 12345',
      JWT_EXPIRY: process.env.JWT_EXPIRY || '1d'
 }
