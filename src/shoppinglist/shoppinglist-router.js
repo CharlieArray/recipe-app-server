5dacaa03465b550cffd3d3978f13b60192943bed
Charles Ybarra <charles.ybarra@outlook.com>
2021-05-14 13:31:16 -0500

    updated shoppinglist router

--- src/shoppinglist/shoppinglist-router.js
+++ src/shoppinglist/shoppinglist-router.js
@@ -8,8 +8,8 @@ const {requireAuth} = require('../middleware/jwt-auth')
 
 /*  HTTP Methods to retrieve shopping list entries from database */
 
-//GET ALL shopping list entries from user in DB (path: /shoppinglist/items/)
-shoppinglistRouter.get('/items/', requireAuth, (req,res, next) => {
+//GET ALL shopping list entries from user in DB (path: /shoppinglist/)
+shoppinglistRouter.get('/', requireAuth, (req,res, next) => {
     const knexInstance = req.app.get('db')
 
     ShoppinglistService.getAllItems(knexInstance, req.user.id)
@@ -21,20 +21,9 @@ shoppinglistRouter.get('/items/', requireAuth, (req,res, next) => {
   })
   
   
-//ADD NEW ITEM to DB for User (path: /shoppinglist/list/:symbol)
-shoppinglistRouter.post('/list/:symbol', bodyParser, requireAuth, (req,res,next)=>{
+//ADD NEW ITEM to DB for User (path: /)
+shoppinglistRouter.post('/', bodyParser, requireAuth, (req,res,next)=>{
   
-  const {symbol} = req.params
-
-      request(`https://finnhub.io/api/v1/quote?symbol=${symbol}&token=c05827n48v6ta8gblt40`, { json: true }, (err, response, body) => {
-        if (err) { return console.log(err); }
-        console.log(body);
-        const {symbol} = req.params;
-        const data = {
-          price: body.c , 
-          ticker: symbol,
-          users_id: req.user.id
-        }
         const knexInstance = req.app.get('db')
         ShoppinglistService.addItem(knexInstance, data)
         .then((data)=>{
@@ -44,11 +33,11 @@ shoppinglistRouter.post('/list/:symbol', bodyParser, requireAuth, (req,res,next)
         .catch(next);
       });
       
-})
+
   
   
 //DELETE ITEM from DB for User (path: /shoppinglist/list/:id)
-shoppinglistRouter.delete( '/list/:id', requireAuth, (req,res,next)=>{
+shoppinglistRouter.delete( '/:id', requireAuth, (req,res,next)=>{
   //id is unique id to that stock
   const {id} = req.params
   const knexInstance = req.app.get('db')
