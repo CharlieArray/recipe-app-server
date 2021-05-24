3f2716a61ef190839274b1a78635efd8352b0fe4
Charles Ybarra <59151304+CharlieArray@users.noreply.github.com>
2021-05-24 02:33:22 -0500

    update test parameter

--- test/app.spec.js
+++ test/app.spec.js
@@ -2,9 +2,9 @@
 const app = require('../src/app')
 
 describe('App', () => {
-  it('GET / responds with 200 containing "Hello, world!"', () => {
+  it('GET / responds with 200 containing "Server is live"', () => {
     return supertest(app)
       .get('/')
-      .expect(200, 'Hello, world!')
+      .expect(200, 'Server is live')
   })
 })
